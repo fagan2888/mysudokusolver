555228256dcca9bd4a8571f19fe6c4d4f9071b41
Jeff Everett <everett.jeff.w@gmail.com>
2015-12-22 17:18:03 -0700

    Reworked to use browserify

--- browserify/page.js
+++ browserify/page.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var solver = require('./solver');
 
 // get all possible inputs
@@ -101,11 +100,3 @@ function solvePuzzle()
     // call solver using input
     // then adjust board
 }
-
-},{"./solver":2}],2:[function(require,module,exports){
-// solve here
-module.exports = function() {
-
-};
-
-},{}]},{},[1]);
---
+++ browserify/solver.js
@@ -0,0 +1,4 @@
+// solve here
+module.exports = function() {
+
+};
